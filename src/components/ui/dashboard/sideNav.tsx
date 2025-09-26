e386e8176094a76f8603e9aa785c2965ff2a6940
NoggyPLZ <brett@bluetractor.com>
2025-09-26 15:12:49 -0500

    fixed the logo to show during dark mode and light mode correctly

--- src/components/ui/dashboard/sideNav.tsx
+++ src/components/ui/dashboard/sideNav.tsx
@@ -19,18 +19,32 @@ export default async function SideNav() {
             className="text-gray-500 p-3 rounded-2xl text-2xl text-center"
           >
             <Image
+              src={`/tech-trap-logos/techtrap-vert-full-logo-lm.webp`}
+              alt={"vertical tech trap logo"}
+              width={100}
+              height={100}
+              className="hidden dark:md:flex"
+            />
+            <Image
+              src={`/tech-trap-logos/techtrap-logo-lm.webp`}
+              height={100}
+              width={100}
+              alt={`Tech Trap logo`}
+              className="hidden dark:flex dark:md:hidden"
+            />
+            <Image
               src={`/tech-trap-logos/techtrap-vert-full-logo.webp`}
               alt={"vertical tech trap logo"}
               width={100}
               height={100}
-              className="hidden md:flex"
+              className="hidden md:flex dark:hidden"
             />
             <Image
               src={`/tech-trap-logos/techtrap-logo.webp`}
               height={100}
               width={100}
               alt={`Tech Trap logo`}
-              className="md:hidden"
+              className="md:hidden dark:hidden"
             />
           </Link>
           <ProfilePreview user={user} />
